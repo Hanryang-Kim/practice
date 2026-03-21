7cbe716b6347d2566e7201bb8c020c18261bd655
ryunhwankim <fbsghksllove@gmail.com>
2026-03-21 17:46:58 +0900

    [feat] day1 - member

--- src/main/java/com/example/practice/api/member/MemberServiceImpl.java
+++ src/main/java/com/example/practice/api/member/MemberServiceImpl.java
@@ -10,6 +10,7 @@ public class MemberServiceImpl implements MemberService{
         memberRepository.save(member);
     }
 
+
     @Override
     public Member findMember(Long memberId) {
         return memberRepository.findById(memberId);
