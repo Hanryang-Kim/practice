f4ff28954d39372ab058044d9e9bea4826569294
ryunhwankim <fbsghksllove@gmail.com>
2026-03-25 17:49:58 +0900

    [feat] day4 - domain

---
+++ src/main/java/hanryang/practice/api/domain/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package hanryang.practice.api.domain.repository;
+
+import hanryang.practice.api.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(String id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
